7696b0a88b2cbcb3dc6979347e4fb8d6bf0b7c51
Lim Xing Zhi <limxz97@gmail.com>
2020-05-05 20:07:06 +0800

    Implemented file upload

--- app.js
+++ app.js
@@ -1,43 +1,52 @@
-// Setting Toggling the alerts
+// const file = document.getElementById("file").files[0];
+
 const errorAlert = '<div class="alert alert-danger"><strong>Parse Error!</strong><br />Check console for error logs<br /><br /><strong>Note:</strong><br />This is using the fetch API to retrieve markdown files.<br />i.e. to say you have to host this web page.</div>'
 
+const input = document.getElementById("input");
+const printButton = document.getElementById('print-btn');
 
-// Phantom file name https://davidwalsh.name/prevent-cache
-const fileName = "./sample.md?" + Date.now()
+(() => {
+  document.getElementById("input").addEventListener("change", () => {
+    printButton.disabled = true;
+    const reader = new FileReader();
+    reader.readAsText(input.files[0]);
 
-fetch(fileName)
-  .then(response => response.text())
-  .then(response => print(response))
-  .then(() => {
-    document.title = document.getElementsByTagName('h1')[0].innerText;
-  })
-  .then(() => {
-    const headingsH1 = document.getElementsByTagName('h1');
-    Array.prototype.map.call(headingsH1, changeToHref);
+    reader.onload = () => {
+      try {
+        startReader(reader.result);
+        printButton.disabled = false;
+      } catch (exception) {
+        document.getElementById('content').innerHTML = errorAlert;
+        console.error(exception);
+      }
+    };
+  }, false);
+})();
 
-    const headingsH2 = document.getElementsByTagName('h2');
-    Array.prototype.map.call(headingsH2, changeToHref);
+const contentDiv = document.getElementById('content');
 
-    const headingsH3 = document.getElementsByTagName('h3');
-    Array.prototype.map.call(headingsH3, changeToHref);
+const startReader = (inputString) => {
 
-    jumpToAnchor();
-  })
-  .catch(error => {
-    console.error(error)
-    print(errorAlert);
-  });
+  // Phantom file name https://davidwalsh.name/prevent-cache
+
+  print(inputString);
+
+  const headingsH1 = contentDiv.getElementsByTagName('h1');
+  Array.prototype.map.call(headingsH1, changeToHref);
+
+  const headingsH2 = contentDiv.getElementsByTagName('h2');
+  Array.prototype.map.call(headingsH2, changeToHref);
+
+  const headingsH3 = contentDiv.getElementsByTagName('h3');
+  Array.prototype.map.call(headingsH3, changeToHref);
+}
 
 const print = (stringInput) => {
-  document.getElementById('content').innerHTML = marked(stringInput)
+  contentDiv.innerHTML = marked(stringInput)
+  document.title = contentDiv.getElementsByTagName('h1')[0].innerText;
 }
 
 const changeToHref = (htmlNode) => {
   const innerHTML = htmlNode.innerHTML;
   htmlNode.innerHTML = '<a href="#' + htmlNode.id + '"><i class="fas fa-link noprint"></i>' + innerHTML + '</a>';
-}
-
-const jumpToAnchor = () => {
-  if (location.hash)
-    document.getElementById(location.hash.slice(1)).getElementsByTagName('a')[0].click();
 }
